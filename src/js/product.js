3b517644ed62255dd71bd253514db9b75a94f22a
ttrampp <ttrampp@msn.com>
2025-03-09 09:59:52 -0600

    in product.js: get cart from local storage and add new products to array= multiple items can be added to cart now

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];     //get cart from local storage..if empty or missing, use array instead
-  cartItems.push(product);                                //adds new product to cartItems array
+  const cartItems = getLocalStorage("so-cart") || []; //get cart from local storage..if empty or missing, use array instead
+  cartItems.push(product); //adds new product to cartItems array
   setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
