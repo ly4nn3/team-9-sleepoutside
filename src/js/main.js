514b8b71d9d6c8c477a763e133a0540646a7f180
Daniel C. Opute <danielcopute@gmail.com>
2025-03-16 19:16:56 +0100

    corrected photo errors and formating

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,13 @@ import { updateCartCount } from "./utils.mjs";
 
 const productCategoryTents = "tents";
 const productTentsData = new ProductData(productCategoryTents);
-const productTentsListElement = document.querySelector('.product-list');
+const productTentsListElement = document.querySelector(".product-list");
 
-const productList = new ProductList(productCategoryTents, productTentsData, productTentsListElement);
+const productList = new ProductList(
+  productCategoryTents,
+  productTentsData,
+  productTentsListElement,
+);
 productList.init();
 
 updateCartCount();
