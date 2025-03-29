7253926e86171814fa3bb70f1fc0700155d74d3f
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-03-28 19:36:21 -0700

    Individual Activity completed c:

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,4 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, alertMessage } from "./utils.mjs";
 import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
@@ -13,7 +13,28 @@ document
 
 // listening for click on the button
 document.querySelector("#checkout-button").addEventListener("click", (e) => {
-  e.preventDefault();
+  e.preventDefault(); //prevents page to reload
 
-  order.checkout();
+  const myForm = document.forms["checkout-form"]; //gets the form element
+
+  const validity = myForm.checkValidity(); //validates if any requirement is missing
+
+  //Add more validation feedback to the user...
+  const cardNumber = document.querySelector("#cardNumber");
+  const expDate = document.querySelector("#expiration");
+
+
+  if (cardNumber.validity.patternMismatch) alertMessage("Invalid card number!");
+  if (expDate.validity.patternMismatch)
+    alertMessage("Invalid expiration date!");
+
+
+  //If any requirement is missing, stops the execution by not calling checkout();
+  if (validity) {
+    order.checkout();
+    myForm.submit(); //submits the form to the url at form.action
+    localStorage.clear(); //Clears the localStorage
+  } else {
+    alertMessage("A value is missing");
+  }
 });
